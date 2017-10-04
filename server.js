4f83872e6018b02892b8e10f6b2415d876832f63
Thulasiraman S <thulasirmn8@gmail.com>
2017-10-04 19:13:14 +0530

    setup start script

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -47,4 +48,4 @@ app.get('/bad', (req, res)=>{
 
 });
 
-app.listen(3000);
+app.listen(port);
